4a3f5821a98c3df877cfc62cd1406c5c6587a1ef
ik1ne <ik1ne@naver.com>
2024-01-11 19:39:23 +0900

    debug 02

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,11 +1,58 @@
 use proc_macro::TokenStream;
 
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, Data, DeriveInput, Field, Fields};
 
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = parse_macro_input!(input as DeriveInput);
+    let input = parse_macro_input!(input as DeriveInput);
 
-    TokenStream::from(quote! {})
+    derive_inner(input)
+        .unwrap_or_else(syn::Error::into_compile_error)
+        .into()
+}
+
+fn derive_inner(input: DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
+    let ident = &input.ident;
+
+    let mut debug_fields: Vec<proc_macro2::TokenStream> = vec![];
+    match input.data {
+        Data::Struct(data) => match data.fields {
+            Fields::Named(fields) => {
+                for field in fields.named.iter() {
+                    debug_fields.push(debug_row(field)?);
+                }
+            }
+            _ => {
+                return Err(syn::Error::new_spanned(
+                    ident,
+                    "derive(CustomDebug) only supports structs with named fields",
+                ))
+            }
+        },
+        _ => {
+            return Err(syn::Error::new_spanned(
+                ident,
+                "derive(CustomDebug) only supports structs",
+            ))
+        }
+    }
+
+    Ok(quote! {
+        impl ::std::fmt::Debug for #ident {
+            fn fmt(&self, fmt: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
+                fmt.debug_struct(stringify!(#ident))
+                    #(#debug_fields)*
+                    .finish()
+            }
+        }
+    })
+}
+
+fn debug_row(field: &Field) -> syn::Result<proc_macro2::TokenStream> {
+    let field_ident = &field.ident;
+
+    Ok(quote! {
+        .field(stringify!(#field_ident), &self.#field_ident)
+    })
 }
--- main.rs
+++ main.rs
@@ -17,44 +17,21 @@
 //     current_dir: Option<String>,
 // }
 
-use quote::ToTokens;
-use syn::{parse_quote, Expr, ExprAssign, Field, Lit};
+use derive_debug::CustomDebug;
 
-fn main() {
-    let incorrect_field: Field = parse_quote! {
-        #[builder(eac = "arg")]
-        args: Vec<String>
-    };
-
-    get_each_attr(&incorrect_field).unwrap();
+#[derive(CustomDebug)]
+pub struct Field {
+    name: &'static str,
+    bitmask: u8,
 }
 
-fn get_each_attr(field: &Field) -> syn::Result<Option<String>> {
-    let Some(attribute) = field
-        .attrs
-        .iter()
-        .find(|attr| attr.path().is_ident("builder"))
-    else {
-        return Ok(None);
+fn main() {
+    let f = Field {
+        name: "F",
+        bitmask: 0b00011100,
     };
 
-    let expr: ExprAssign = attribute.parse_args()?;
+    let debug = format!("{:?}", f);
 
-    eprintln!("{:?}", expr.left.as_ref().to_token_stream());
-
-    let Expr::Path(path_left) = expr.left.as_ref() else {
-        panic!("builder attribute must be literal");
-    };
-
-    panic!("{:?}", path_left.path.to_token_stream());
-
-    let Expr::Lit(lit_right) = expr.right.as_ref() else {
-        panic!("builder attribute must be literal");
-    };
-
-    let Lit::Str(lit_str) = &lit_right.lit else {
-        panic!("builder attribute must be string literal");
-    };
-
-    Ok(Some(lit_str.value()))
+    assert!(debug.starts_with(r#"Field { name: "F","#));
 }
