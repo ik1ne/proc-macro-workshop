fe1e6fa257a272e221ca5e1504b0bc9131d00212
ik1ne <ik1ne@naver.com>
2024-01-11 19:01:56 +0900

    debug 1

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,11 @@
 use proc_macro::TokenStream;
 
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let _ = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    TokenStream::from(quote! {})
 }
