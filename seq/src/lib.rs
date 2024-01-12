fa45a7e8a83e833936ee5bbfe4e4eb122a643a40
ik1ne <ik1ne@naver.com>
2024-01-12 19:01:20 +0900

    seq 01

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,15 @@
 use proc_macro::TokenStream;
+use quote::quote;
+
+use syn::parse_macro_input;
+
+use crate::seq::Seq;
+
+mod seq;
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let _seq = parse_macro_input!(input as Seq);
 
-    unimplemented!()
+    TokenStream::from(quote! {})
 }
---
+++ seq/src/seq.rs
@@ -0,0 +1,24 @@
+use syn::parse::{Parse, ParseStream};
+use syn::{Block, Ident, Lit, Token};
+
+pub(crate) struct Seq {
+    i: Ident,
+    _in: Token![in],
+    range_begin: Lit,
+    _dot_dot: Token![..],
+    range_end: Lit,
+    body: Block,
+}
+
+impl Parse for Seq {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        Ok(Seq {
+            i: input.parse()?,
+            _in: input.parse()?,
+            range_begin: input.parse()?,
+            _dot_dot: input.parse()?,
+            range_end: input.parse()?,
+            body: input.parse()?,
+        })
+    }
+}
