5ca735d6f07e6d6e3d2d4d061266a263bf182d78
ik1ne <ik1ne@naver.com>
2024-01-12 19:27:29 +0900

    refactor

--- main.rs
+++ main.rs
@@ -17,44 +17,16 @@
 //     current_dir: Option<String>,
 // }
 
-use quote::ToTokens;
-use syn::{parse_quote, Expr, ExprAssign, Field, Lit};
+use seq::seq;
 
-fn main() {
-    let incorrect_field: Field = parse_quote! {
-        #[builder(eac = "arg")]
-        args: Vec<String>
+macro_rules! expand_to_nothing {
+    ($arg:literal) => {
+        // nothing
     };
-
-    get_each_attr(&incorrect_field).unwrap();
 }
 
-fn get_each_attr(field: &Field) -> syn::Result<Option<String>> {
-    let Some(attribute) = field
-        .attrs
-        .iter()
-        .find(|attr| attr.path().is_ident("builder"))
-    else {
-        return Ok(None);
-    };
+seq!(N in 0..4 {
+    expand_to_nothing!(N);
+});
 
-    let expr: ExprAssign = attribute.parse_args()?;
-
-    eprintln!("{:?}", expr.left.as_ref().to_token_stream());
-
-    let Expr::Path(path_left) = expr.left.as_ref() else {
-        panic!("builder attribute must be literal");
-    };
-
-    panic!("{:?}", path_left.path.to_token_stream());
-
-    let Expr::Lit(lit_right) = expr.right.as_ref() else {
-        panic!("builder attribute must be literal");
-    };
-
-    let Lit::Str(lit_str) = &lit_right.lit else {
-        panic!("builder attribute must be string literal");
-    };
-
-    Ok(Some(lit_str.value()))
-}
+fn main() {}
--- seq/src/seq.rs
+++ seq/src/seq.rs
@@ -1,6 +1,7 @@
 use quote::quote;
 use syn::parse::{Parse, ParseStream};
-use syn::{Block, Ident, Lit, Token};
+use syn::token::Brace;
+use syn::{braced, Ident, Lit, Token};
 
 pub(crate) struct Seq {
     i: Ident,
@@ -8,24 +9,27 @@ pub(crate) struct Seq {
     range_begin: Lit,
     _dot_dot: Token![..],
     range_end: Lit,
-    body: Block,
+    brace_token: Brace,
+    body: proc_macro2::TokenStream,
 }
 
 impl Parse for Seq {
     fn parse(input: ParseStream) -> syn::Result<Self> {
+        let content;
         Ok(Seq {
             i: input.parse()?,
             _in: input.parse()?,
             range_begin: input.parse()?,
             _dot_dot: input.parse()?,
             range_end: input.parse()?,
-            body: input.parse()?,
+            brace_token: braced!(content in input),
+            body: content.parse()?,
         })
     }
 }
 
 impl Seq {
     pub(crate) fn expand(self) -> syn::Result<proc_macro2::TokenStream> {
-        Ok(quote!())
+        Ok(quote! {})
     }
 }
