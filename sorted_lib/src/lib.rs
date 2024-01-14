62e478412413068762c6c3b794447d59bce0e6aa
ik1ne <ik1ne@naver.com>
2024-01-14 18:16:33 +0900

    sorted 01

--- sorted_lib/src/lib.rs
+++ sorted_lib/src/lib.rs
@@ -1,6 +1,13 @@
+use quote::quote;
+use syn::Item;
+
 pub fn derive(
-    args: proc_macro2::TokenStream,
+    _args: proc_macro2::TokenStream,
     input: proc_macro2::TokenStream,
 ) -> syn::Result<proc_macro2::TokenStream> {
-    todo!()
+    let item: Item = syn::parse2(input)?;
+
+    Ok(quote! {
+        #item
+    })
 }
