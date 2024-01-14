d4b4e0d63067612e6df0805ccc1a733492399dd3
ik1ne <ik1ne@naver.com>
2024-01-14 18:33:11 +0900

    sorted 03

--- sorted_lib/src/lib.rs
+++ sorted_lib/src/lib.rs
@@ -1,5 +1,5 @@
 use quote::quote;
-use syn::Item;
+use syn::{Item, ItemEnum};
 
 pub fn derive(
     _args: proc_macro2::TokenStream,
@@ -7,14 +7,44 @@ pub fn derive(
 ) -> syn::Result<proc_macro2::TokenStream> {
     let item: Item = syn::parse2(input)?;
 
-    let Item::Enum(item_enum) = item else {
-        return Err(syn::Error::new(
-            proc_macro2::Span::call_site(),
-            "expected enum or match expression",
-        ));
+    match &item {
+        Item::Enum(item_enum) => {
+            check_enum_sorted(item_enum)?;
+        }
+        _ => {
+            return Err(syn::Error::new(
+                proc_macro2::Span::call_site(),
+                "expected enum or match expression",
+            ))
+        }
     };
 
     Ok(quote! {
-        #item_enum
+        #item
     })
+}
+
+fn check_enum_sorted(item_enum: &ItemEnum) -> syn::Result<()> {
+    let mut sorted_order = item_enum.variants.iter().collect::<Vec<_>>();
+    sorted_order.sort_by_key(|variant| &variant.ident);
+
+    item_enum
+        .variants
+        .iter()
+        .zip(sorted_order.iter())
+        .try_for_each(|(variant, sorted_variant)| {
+            if variant.ident != sorted_variant.ident {
+                Err(syn::Error::new(
+                    sorted_variant.ident.span(),
+                    format!(
+                        "{} should sort before {}",
+                        sorted_variant.ident, variant.ident,
+                    ),
+                ))
+            } else {
+                Ok(())
+            }
+        })?;
+
+    Ok(())
 }
