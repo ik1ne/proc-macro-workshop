a6b92cc45857b92c7814a92b3fb342f1f7120e63
ik1ne <ik1ne@naver.com>
2024-01-14 18:24:21 +0900

    sorted 02

--- sorted_lib/src/lib.rs
+++ sorted_lib/src/lib.rs
@@ -7,7 +7,14 @@ pub fn derive(
 ) -> syn::Result<proc_macro2::TokenStream> {
     let item: Item = syn::parse2(input)?;
 
+    let Item::Enum(item_enum) = item else {
+        return Err(syn::Error::new(
+            proc_macro2::Span::call_site(),
+            "expected enum or match expression",
+        ));
+    };
+
     Ok(quote! {
-        #item
+        #item_enum
     })
 }
