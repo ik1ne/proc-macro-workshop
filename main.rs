8ca1c080698582be356959b43c5f0895058c46a1
ik1ne <ik1ne@naver.com>
2024-01-14 18:09:28 +0900

    sorted prep

--- main.rs
+++ main.rs
@@ -1,18 +1,15 @@
 use quote::quote;
-use std::str::FromStr;
 
 fn main() {
-    let input = proc_macro2::TokenStream::from_str(
-        r#"
-pub enum Conference {
-    RustBeltRust,
-    RustConf,
-    RustFest,
-    RustLatam,
-    RustRush,
-}"#,
-    )
-    .unwrap();
+    let input = quote! {
+        pub enum Conference {
+            RustBeltRust,
+            RustConf,
+            RustFest,
+            RustLatam,
+            RustRush,
+        }
+    };
 
     println!("{}", sorted_lib::derive(quote! {}, input).unwrap());
 }
