f2d6421b934cf4d27f8965c8616914e36e62dbef
ik1ne <ik1ne@naver.com>
2024-01-20 18:24:56 +0900

    bitfield 09

--- bitfield_lib/src/checks.rs
+++ bitfield_lib/src/checks.rs
@@ -7,39 +7,39 @@ pub enum FiveMod8 {}
 pub enum SixMod8 {}
 pub enum SevenMod8 {}
 
-pub trait GetMarker {
+pub trait GetModular {
     type Marker;
 }
 
-impl GetMarker for [(); 0] {
+impl GetModular for [(); 0] {
     type Marker = ZeroMod8;
 }
 
-impl GetMarker for [(); 1] {
+impl GetModular for [(); 1] {
     type Marker = OneMod8;
 }
 
-impl GetMarker for [(); 2] {
+impl GetModular for [(); 2] {
     type Marker = TwoMod8;
 }
 
-impl GetMarker for [(); 3] {
+impl GetModular for [(); 3] {
     type Marker = ThreeMod8;
 }
 
-impl GetMarker for [(); 4] {
+impl GetModular for [(); 4] {
     type Marker = FourMod8;
 }
 
-impl GetMarker for [(); 5] {
+impl GetModular for [(); 5] {
     type Marker = FiveMod8;
 }
 
-impl GetMarker for [(); 6] {
+impl GetModular for [(); 6] {
     type Marker = SixMod8;
 }
 
-impl GetMarker for [(); 7] {
+impl GetModular for [(); 7] {
     type Marker = SevenMod8;
 }
 
@@ -51,4 +51,29 @@ impl TotalSizeIsMultipleOfEightBits for ZeroMod8 {
     type _Check = ();
 }
 
-pub type MultipleOfEight<T> = <<T as GetMarker>::Marker as TotalSizeIsMultipleOfEightBits>::_Check;
+pub type MultipleOfEight<T> = <<T as GetModular>::Marker as TotalSizeIsMultipleOfEightBits>::_Check;
+
+pub type InRange<T> = <<T as GetBool>::Marker as DiscriminantInRange>::_Check;
+
+pub trait GetBool {
+    type Marker;
+}
+
+pub enum True {}
+pub enum False {}
+
+impl GetBool for [(); 1] {
+    type Marker = True;
+}
+
+impl GetBool for [(); 0] {
+    type Marker = False;
+}
+
+pub trait DiscriminantInRange {
+    type _Check;
+}
+
+impl DiscriminantInRange for True {
+    type _Check = ();
+}
--- bitfield_lib/src/lib.rs
+++ bitfield_lib/src/lib.rs
@@ -1,8 +1,10 @@
+use proc_macro2::Ident;
+use quote::{format_ident, quote, quote_spanned};
+use syn::spanned::Spanned;
+use syn::{ItemEnum, Variant};
+
 pub use derive_bitfield::*;
-use proc_macro2::TokenStream;
-use quote::{format_ident, quote};
 pub use specifier::*;
-use syn::ItemEnum;
 
 mod derive_bitfield;
 mod specifier;
@@ -24,12 +26,17 @@ pub fn derive_bitfield_specifier(
 
     if num_fields.next_power_of_two() != num_fields {
         return Err(syn::Error::new_spanned(
-            TokenStream::new(),
+            proc_macro2::TokenStream::new(),
             "BitfieldSpecifier expected a number of variants which is a power of 2",
         ));
     }
 
     let b_ty = format_ident!("B{}", bytes);
+
+    let variant_in_range_checks = variants
+        .iter()
+        .enumerate()
+        .map(|(i, variant)| generate_variant_in_range_check(&ident, variant, i, num_fields));
 
     let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
@@ -50,6 +57,28 @@ pub fn derive_bitfield_specifier(
                 let value = value as <#b_ty as ::bitfield::ValueGetSet>::ValueType;
                 <#b_ty as ::bitfield::ValueGetSet>::set(arr, offset, value);
             }
+
+        }
+
+        impl #impl_generics #ident #ty_generics #where_clause {
+            #(#variant_in_range_checks)*
         }
     })
+}
+
+fn generate_variant_in_range_check(
+    enum_ident: &Ident,
+    variant: &Variant,
+    i: usize,
+    num_fields: usize,
+) -> proc_macro2::TokenStream {
+    let ident = &variant.ident;
+    let fn_name = format_ident!("_check_{}", i);
+
+    quote_spanned! {variant.span()=>
+        fn #fn_name() {
+            const IS_IN_RANGE: usize = ((#enum_ident::#ident as usize) < #num_fields) as usize;
+            let _: bitfield::checks::InRange<[(); IS_IN_RANGE]> = ();
+        }
+    }
 }
